275247ec22d75a4fb40c52b7dc931a43c1d38176
Lucas Lima <lcs.freirelima@gmailcom>
2018-09-19 01:56:49 -0300

    Project beginning

---
+++ src/main/java/limaflucas/reativespring/ReativeSpringApplication.java
@@ -0,0 +1,12 @@
+package limaflucas.reativespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativeSpringApplication.class, args);
+	}
+}
